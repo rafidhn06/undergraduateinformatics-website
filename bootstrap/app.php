a180519b71773d9538762d396543a71cb3ad8edb
rafidhn06 <rafidhn06@gmail.com>
2026-09-15 06:35:57 +0700

    feat: bind public path from env for split deploy layout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+if (($publicPath = $_ENV['PUBLIC_PATH'] ?? null) !== null && $publicPath !== '') {
+    $app->usePublicPath($publicPath);
+}
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
